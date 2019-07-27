11361616ecdd72d2ed5fccbcccd3933093c60870
WhiteN <whiten.ncs@gmail.com>
2019-07-27 23:35:08 +0300

    burger-menu to mobile brackpoints

---
+++ src/js/header.js
@@ -0,0 +1,16 @@
+
+;(function () {
+
+    // Mobile Menu
+    $(".menu__btn-mobile").on("touchend", e => {
+        $('.menu__item--mobile-active').toggleClass('menu__item--mobile-active');
+        $(".menu__items").toggleClass('menu__items--mobile-active');
+    });
+
+    $(".menu__item").on("touchend", e => {
+        $('.menu__item--mobile-active').toggleClass('menu__item--mobile-active');
+        $(e.target).toggleClass('menu__item--mobile-active');
+        return false
+    });
+
+})();
